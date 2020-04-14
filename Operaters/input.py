82b1ed7bf175227dc73586fb34fe5fd404eab127
Devamakesh <47493737+devamakeshr@users.noreply.github.com>
2020-04-14 22:18:09 +0530

    add fill-operater

---
+++ Operaters/input.py
@@ -0,0 +1,10 @@
+# plus operater in Python(+)
+n = 6
+b = 9
+print("this add in n+b " + str(n+b))
+
+# module operater in Python (%)
+N = 6
+M = 20
+print(N % M)
+print(M % N)
--- input.py
+++
@@ -1,6 +0,0 @@
-import string
-
-i = str(input("get first string ?"))
-j = str(input("second string ?"))
-
-print(i is j)
